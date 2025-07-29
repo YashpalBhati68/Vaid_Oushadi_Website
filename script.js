7413505994c7f8df8913da524273f46f2c03cd50
Yashpal Bhati <codercollabnetwork@gmail.com>
2025-07-29 22:16:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const header = document.querySelector(".header");
+const toggleClass = "is-sticky";
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll > 150) {
+    header.classList.add(toggleClass);
+  } else {
+    header.classList.remove(toggleClass);
+  }
+});
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
